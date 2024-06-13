b056bfe48d0b54b8096c37a32c2d554f1b1df3bb
CaoNgocQuyIT2k2 <caongocquyit2k2@gmail.com>
2024-06-13 14:55:20 +0700

    first

--- src/components/post/layout/PostLayoutNewest.jsx
+++ src/components/post/layout/PostLayoutNewest.jsx
@@ -13,7 +13,7 @@ const PostLayoutNewest = ({ postSizeMd, postBgDark }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/api/Top5Newest");
+        const response = await axios.get(`/api/Top5Newest?t=${new Date().getTime()}`);
         setData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
--- src/pages/api/latesDanTri.js
+++ src/pages/api/latesDanTri.js
@@ -9,7 +9,14 @@ export default async function fetchLatestDantri(req, res) {
 
   // Handle API request
   try {
-    const response = await axios.get("http://ec2-18-143-143-173.ap-southeast-1.compute.amazonaws.com:8080/api/v1/article/anonymous/get-latest-dantri?count=6");
+    const response = await axios.get(
+      "http://ec2-18-143-143-173.ap-southeast-1.compute.amazonaws.com:8080/api/v1/article/anonymous/get-latest-dantri?count=6",
+      {
+        headers: {
+          'Cache-Control': 'no-cache' // Disable cache for Axios request
+        }
+      }
+    );
     const data = response.data;
     console.log("🚀 ~ data:", data);
     if (response.status === 200) {
@@ -18,7 +25,7 @@ export default async function fetchLatestDantri(req, res) {
       throw new Error('Unexpected status code from API');
     }
   } catch (error) {
-    console.log("🚀 ~ error:", error);
+    console.error("Error fetching data:", error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 }
